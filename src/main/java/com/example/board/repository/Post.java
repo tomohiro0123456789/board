06b3690c94b0f5950740dc73889516a37bf65bf8
tomohiro <tomohiro0123456789@gmail.com>
2022-07-07 00:43:11 +0900

    commit

--- src/main/java/com/example/board/repository/Post.java
+++ src/main/java/com/example/board/repository/Post.java
@@ -1,5 +1,8 @@
 package com.example.board.repository;
 
+import com.example.board.validation.Group1;
+import com.example.board.validation.Group2;
+
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -21,37 +24,37 @@ import javax.validation.constraints.Size;
 @Data
 public class Post {
 
-    /** ID */
-    @Id
-    @Column
-    @NotNull
-    private String id = null;
+	/** ID */
+	@Id
+	@Column
+	@NotNull
+	private String id = null;
 
-    /** 投稿者 */
-    @NotEmpty
-    @Size(min = 1, max = 20)
-    @Column(length = 20, nullable = false)
-    private String author = null;
+	/** 投稿者 */
 
-    /** タイトル */
-    @NotEmpty
-    @Size(min = 1, max = 20)
-    @Column(length = 20, nullable = false)
-    private String title = null;
+	@NotEmpty(groups = Group1.class)
+	@Size(min = 1, max = 20, groups = Group2.class)
+	private String author = null;
 
-    /** 内容 */
-    @NotEmpty
-    @Size(min = 1, max = 1000)
-    @Column(length = 1000, nullable = false)
-    private String body = null;
+	/** タイトル */
 
-    /** 登録日時 */
-    private Date createdDate = null;
+	@NotEmpty(groups = Group1.class)
+	@Size(min = 1, max = 20, groups = Group2.class)
+	private String title = null;
 
-    /** 更新日時 */
-    private Date updatedDate = null;
+	/** 内容 */
 
-    /** 削除済 */
-    private boolean deleted = false;
+	@NotEmpty(groups = Group1.class)
+	@Size(min = 1, max = 1000, groups = Group2.class)
+	private String body = null;
+
+	/** 登録日時 */
+	private Date createdDate = null;
+
+	/** 更新日時 */
+	private Date updatedDate = null;
+
+	/** 削除済 */
+	private boolean deleted = false;
 
 }
