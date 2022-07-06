8d2ea14f9ab5f4ac40c7df2d6f71a2c84b2f1a7c
tomohiro <tomohiro0123456789@gmail.com>
2022-07-06 19:11:41 +0900

    complete

--- src/main/java/com/example/board/repository/Post.java
+++ src/main/java/com/example/board/repository/Post.java
@@ -9,6 +9,10 @@ import javax.persistence.Table;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * 投稿.
  */
@@ -20,17 +24,24 @@ public class Post {
     /** ID */
     @Id
     @Column
+    @NotNull
     private String id = null;
 
     /** 投稿者 */
+    @NotEmpty
+    @Size(min = 1, max = 20)
     @Column(length = 20, nullable = false)
     private String author = null;
 
     /** タイトル */
+    @NotEmpty
+    @Size(min = 1, max = 20)
     @Column(length = 20, nullable = false)
     private String title = null;
 
     /** 内容 */
+    @NotEmpty
+    @Size(min = 1, max = 1000)
     @Column(length = 1000, nullable = false)
     private String body = null;
 
